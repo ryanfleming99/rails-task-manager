aad013e6fb6a4598e4703a6b69e7aa56e623f912
Ryan Fleming <ryanfleming93@aol.com>
2019-11-12 16:29:07 +0000

    Challenge almost done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # see all tasks
+  get 'tasks', to: 'tasks#index'
+
+  # add a task
+  get 'tasks/new', to: 'tasks#new', as: :new
+  post 'tasks', to: 'tasks#create', as: :create
+
+  # show info about one task
+  get 'tasks/:id', to: 'tasks#show', as: :show
+
+  # edit a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  # update a task
+  patch 'tasks', to: 'tasks#update', as: :update
+
+  # delete/destroy a task
+  get 'destroy', to: 'tasks#destroy'
 end
